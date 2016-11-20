aa4fafc36d5bc8f4e1f41482b3b4784f803370f8
limerentfox <christi@limerentfox.com>
2016-11-20 02:33:40 -0500

    programming-languages lab complete

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+
 def reformat_languages(languages)
-  # your code here
+    new_hash = {}
+      languages.each do |style, language_hash|
+        language_hash.each do |language, info|
+          if new_hash[language].nil?
+             new_hash[language] = info
+             new_hash[language][:style] = [style]
+           else
+             new_hash[language][:style] << style
+          end
+       end
+     end
+     return new_hash
 end
